d70acba67dd98cedb2a070338dbaf1c3e3904682
MrSamy91 <samy.ajouid@gmail.com>
2023-10-13 03:42:36 -0700

    4-puts.c 6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
-
-/* main - Script used to print a message without use printf */
-
+/**
+ * main - Script used to print a message without use printf
+ *
+ * Return: 0 = success
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
