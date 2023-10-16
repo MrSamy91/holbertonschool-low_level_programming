ee97e9793db34acdeefc45699e32c2657d504b31
MrSamy91 <samy.ajouid@gmail.com>
2023-10-16 07:48:53 -0700

    2-print_alphabet.c 1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main (void)
+{
+	char letter = 'a'
+	
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
+
+return(0);
+}
