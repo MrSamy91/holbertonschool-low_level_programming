f59b2b9087b308e818b5acfeec2d8371f9239cb0
MrSamy91 <samy.ajouid@gmail.com>
2023-10-16 01:51:38 -0700

    task 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function initializes a random number generator,
+ * generates a random number, and prints whether the number is positive,
+ * zero, or negative.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+
+
+
+
+	return (0);
+}
