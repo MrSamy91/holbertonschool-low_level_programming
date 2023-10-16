9ecebf07069da6aa36c2880662160bf309ead327
MrSamy91 <samy.ajouid@gmail.com>
2023-10-16 07:24:02 -0700

    1-last_digit.c 9

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 
-
+/**
+  * main - Entry point
+  *
+  * Return: always 0 (success)
+  */
 
 /* betty style doc for function main goes there */
 int main(void)
