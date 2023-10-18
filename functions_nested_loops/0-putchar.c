daeebcb14ce72ea41cd9b2c2f5c4ff3175400d83
MrSamy91 <samy.ajouid@gmail.com>
2023-10-18 05:35:09 -0700

    0-putchar 1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: always 0 (success)
+  */
+
+/* betty style doc for function main goes there */
+
+
+int main ()
+{
+
+	printf("_putchar\n");
+
+return(0);
+}
