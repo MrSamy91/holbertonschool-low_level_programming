e0513490fb5b2bb0783eacf0fa27220f42aa87dd
MrSamy91 <samy.ajouid@gmail.com>
2023-10-19 07:37:52 -0700

    4-isalpha.c 1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _isalpha - check if c is alpha*
+ * @c: The ASCII value of the character to be checked.
+ * Description: check if c is alpha
+ * Return: 1 if lowercase or 0 if uppercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+
+}
