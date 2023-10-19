7841488ee11eefac448a6e4f01317c6fa34291be
MrSamy91 <samy.ajouid@gmail.com>
2023-10-19 05:01:07 -0700

    1-alphabet.c 3

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -13,10 +13,7 @@
 
 void print_alphabet(void)
 {
-
-	char letter = 'a';
-
-	for(letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 		_putchar('\n');
 }
