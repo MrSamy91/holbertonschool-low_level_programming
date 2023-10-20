e9f65c466ba51bcee74bfd842c4b027186a84628
MrSamy91 <samy.ajouid@gmail.com>
2023-10-20 09:23:52 -0700

    8-24_hours.c 6

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,14 +9,19 @@
 void jack_bauer(void)
 {
 	int minutes, hours;
+
 	{
 	for (hours = 0; hours < 24; hours++)
-	for (minutes = 0; minutes < 60; minutes++)
-	_putchar('0' + (hours / 10));
+	{
+		for (minutes = 0; minutes < 60; minutes++)
+		{
+			_putchar('0' + (hours / 10));
 	_putchar('0' + (hours % 10));
 	_putchar(':');
 	_putchar('0' + (minutes / 10));
 	_putchar('0' + (minutes % 10));
 	_putchar('\n');
+}
+}
 }
 }
