4a195798e9683aa5246e5e4b67d713e93ba20cb6
MrSamy91 <samy.ajouid@gmail.com>
2023-10-20 07:29:01 -0700

    7-print_last_digit.c 1

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - entry point
+ * @num: the number to test
+ * Description: absolute value of an integer.
+ * Return: lidigit if success
+ */
+
+int print_last_digit(int num)
+{
+	int ldigit = num % 10;
+		return (ldigit);
+
+}
