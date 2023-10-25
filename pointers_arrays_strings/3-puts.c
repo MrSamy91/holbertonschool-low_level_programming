950b840d0f19e4086baa9625f85e91a6a9aa9819
MrSamy91 <samy.ajouid@gmail.com>
2023-10-25 06:29:06 -0700

    3-puts.c 1

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+  * _puts - Entry point
+  * @str: variable
+  * Return: always 0 (success)
+  */
+
 void _puts(char *str)
 {
+	int i = 0;
 
-	_puts("hello");
+	for (; str[i]; i++)
 
+	_putchar(str[i]);
 }
