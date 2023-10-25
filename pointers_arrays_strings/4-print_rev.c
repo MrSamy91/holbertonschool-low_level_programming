74381e31f908a622762344ca9d81d55293d48bd7
MrSamy91 <samy.ajouid@gmail.com>
2023-10-25 06:49:21 -0700

    4-print_rev.c 1

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+
+
+
+void print_rev(char *s)
+{
+        int i = '\0';
+
+        for (; str[i]; i--)
+		_putchar(str[i]);
+	_putchar('\n');
+}
