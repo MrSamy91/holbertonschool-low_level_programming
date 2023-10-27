5cad2486bf93fb3820439f5a71cf9892e45376a0
MrSamy91 <samy.ajouid@gmail.com>
2023-10-27 03:18:39 -0700

    6-puts2.c 1

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - entry point*
+ * @str: pointer to an string
+ * Description: function that prints every other character of a string.
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+sh: 1: q: not found
+sh: 1: q: not found
+	putchar('\n');
+}
