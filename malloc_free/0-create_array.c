960211508dfcb1fe2e7a57426c6faedf0ebea873
MrSamy91 <samy.ajouid@gmail.com>
2023-11-10 08:36:51 -0800

    0-create_array.c 3

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -15,12 +15,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *tableau;
 
-	if(size == 0)
+	if (size == 0)
 		return (NULL);
 	tableau = malloc(sizeof(char) * size);
 	if (tableau == 0)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		tableau[i] = c;
-	return(tableau);
+	return (tableau);
 }
