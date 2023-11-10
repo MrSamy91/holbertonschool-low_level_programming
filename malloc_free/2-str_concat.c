7ea1b58473f59e3e900c99973752fb6ea2336d43
MrSamy91 <samy.ajouid@gmail.com>
2023-11-10 11:22:55 -0800

    2-str_concat.c 1

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - function
+ * @s1: char ptr
+ * @s2: char ptr
+ *
+ * Return: char ptr
+ */
+char	*str_concat(char *s1, char *s2)
+{
+	char	*r;
+	int	l;
+	int	x;
+
+	if (s1 == 0 && s2 == 0)
+	{
+		r = (char *) malloc(sizeof(char) * 1);
+		if (r == 0)
+			return (0);
+		r[0] = '\0';
+		return (r);
+	}
+	if (s1 == 0)
+		return (_strdup(s2));
+	if (s2 == 0)
+		return (_strdup(s1));
+	l = 0;
+	for (x = 0; s1[x]; x++)
+		l++;
+	for (x = 0; s2[x]; x++)
+		l++;
+	r = (char *) malloc(sizeof(char) * (l + 1));
+	if (r == 0)
+		return (0);
+	l = 0;
+	for (x = 0; s1[x]; x++)
+	{
+		r[l] = s1[x];
+		l++;
+	}
+	for (x = 0; s2[x]; x++)
+	{
+		r[l] = s2[x];
+		l++;
+	}
+	r[l] = '\0';
+	return (r);
+}
