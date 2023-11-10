bb2f3a15ae3ee8ed06634bf6acecdaf9fb0f5ea1
MrSamy91 <samy.ajouid@gmail.com>
2023-11-10 08:21:09 -0800

    0-create_array.c

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+char *create_array(unsigned int size, char c)
+{
+
+	unsigned int i;
+	char *tableau
+
+	if(size == 0)
+		return (NULL);
+
+tableau = malloc(sizeof(char) * size);
+	if (tableau == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		tableau[i] = c;
+
+	return (tableau);
+}
+
+
+}
