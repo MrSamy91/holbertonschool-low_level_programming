c1bef0c6182a9ce56c5cd89e610b5a1c14a18f18
MrSamy91 <samy.ajouid@gmail.com>
2023-11-10 11:13:23 -0800

    1-strdup.c 2

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,15 +11,18 @@ char *_strdup(char *str)
 {
 	char *res;
 	int x;
-	
+
 	if (str == 0)
 		return (0);
-	for (x = 0; str[x]; x++);
+	for (x = 0; str[x]; x++)
+
 	res = (char *) malloc(sizeof(char) * (x + 1));
+
 	if (res == 0)
 		return (0);
-	for (x = 0; str[x]; x++);
+	for (x = 0; str[x]; x++)
 	res[x] = str[x];
 	res[x] = '\0';
+	printf("Duplicated");
 	return (res);
 }
