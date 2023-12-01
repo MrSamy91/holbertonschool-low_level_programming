bf45f3ad2155ff0e2bc69dc65a925a92a0702628
MrSamy91 <samy.ajouid@gmail.com>
2023-12-01 09:08:27 -0800

    adding extra task

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*
+* before_main - is a function executed before the main function.
+* Description:
+* This function is automatically executed before the main function*
+ */
+
 void __attribute__((constructor)) before_main()
 {
-	    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
 
